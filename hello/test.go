567317d8200c2cb1ea38900e3ff93d51e04c1904
Mike Hewitt <MatrixMike@users.noreply.github.com>
2016-12-07 22:14:06 +1100

    added files via drag

---
+++ hello/test.go
@@ -0,0 +1,22 @@
+package main
+// 06.12.2016 21:22:11   initial Go test <Mike Hewitt>
+import (
+	"fmt"
+
+	"github.com/matrixmike/stringutil"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+		fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println("VK3KMJ")
+		fmt.Println(stringutil.Reverse("leahciM"))
+	fmt.Println("Hello, 世界")
+}
+
+// as a memo for later
+// https://godoc.org/golang.org/x/mobile/app
+
+// https://golang.org/doc/code.html#Command
+
+
