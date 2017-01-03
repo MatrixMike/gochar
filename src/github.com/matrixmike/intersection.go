190cd9f6556944ae2d0d2f39e2181ddef4db7285
Mike <matrixmike@icloud.com>
2017-01-03 20:51:30 +1100

    alignment 1

--- src/github.com/matrixmike/intersection.go
+++ src/github.com/matrixmike/intersection.go
@@ -6,10 +6,15 @@ import (
 )
 
 func main() {
-
+	var a [4]string
+	a[0] = "Hello"
+	a[1] = "World"
+	a[2] = "third"
+	a[3] = "capacitor" 
+	
 	fmt.Println(strings.IndexAny("chicken", "aeiouy"))
 	fmt.Println(strings.IndexAny("crwth", "aeiouy"))
-	fmt.Println(strings.IndexAny("capacitor", "resistor"))
+	fmt.Println(strings.IndexAny(a[3], "resistor")         )
 	fmt.Println(strings.IndexAny("capacitor", "condensor"))
 	fmt.Println(strings.IndexAny("apacitor", "ondensor"))
 }
