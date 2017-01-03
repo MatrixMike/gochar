c8a04657b77e1bacad0584b117e836e4c62d38eb
Mike <matrixmike@icloud.com>
2017-01-03 22:05:49 +1100

    crude testing of intersection

--- src/github.com/matrixmike/intersection.go
+++ src/github.com/matrixmike/intersection.go
@@ -9,13 +9,38 @@ func main() {
 	var a [4]string
 	a[0] = "Hello"
 	a[1] = "World"
+	
 	a[2] = "resistor"
 	a[3] = "capacitor" 
+	x := 0
+	//x := 1
+	//	var i interface{}
+	//	i = 42
+	//describe(i)
+	x = x +1 
+
 	
 	fmt.Println(strings.IndexAny("chicken", "aeiouy"))
 	fmt.Println(strings.IndexAny("crwth", "aeiouy"))
+	
 	fmt.Println(strings.IndexAny(a[3], a[2])     , "K"    )
-	        fmt.Println(string([]rune(a[0])[4]))
+		x = strings.IndexAny(a[3], a[2])
+		
+		fmt.Println(a[2:4])
+		fmt.Print("Index of intersecting character ",x, " ")
+	fmt.Println(string([]rune(a[3])  [x]  ))
+	a[2] = "transistor"
+	a[3] = "capacitor" 	
+	fmt.Println(strings.IndexAny(a[3], a[2])     , "K"    )
+		x = strings.IndexAny(a[3], a[2])
+		
+		fmt.Println(a[2:4])
+		fmt.Print("Index of intersecting character ",x, " ")
+	fmt.Println(string([]rune(a[3])  [x]  ))	
+	
+	
+	
+	
 	fmt.Println(strings.IndexAny("capacitor", "condensor"))
 	fmt.Println(strings.IndexAny("apacitor", "ondensor"))
 }
