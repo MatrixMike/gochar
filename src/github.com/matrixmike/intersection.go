88b75157b5d92f957f363f48364cd796c52403e9
Mike <matrixmike@icloud.com>
2017-01-03 20:54:06 +1100

    swapped string for arry usage

--- src/github.com/matrixmike/intersection.go
+++ src/github.com/matrixmike/intersection.go
@@ -9,12 +9,12 @@ func main() {
 	var a [4]string
 	a[0] = "Hello"
 	a[1] = "World"
-	a[2] = "third"
+	a[2] = "resistor"
 	a[3] = "capacitor" 
 	
 	fmt.Println(strings.IndexAny("chicken", "aeiouy"))
 	fmt.Println(strings.IndexAny("crwth", "aeiouy"))
-	fmt.Println(strings.IndexAny(a[3], "resistor")         )
+	fmt.Println(strings.IndexAny(a[3], a[2])         )
 	fmt.Println(strings.IndexAny("capacitor", "condensor"))
 	fmt.Println(strings.IndexAny("apacitor", "ondensor"))
 }
