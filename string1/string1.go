11fec68ef89cfb5f013a09ed184d96f171a37ae8
Mike <matrixmike@icloud.com>
2017-01-03 20:34:37 +1100

    added some examples

---
+++ string1/string1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [3]string
+	a[0] = "Hello"
+	a[1] = "World"
+	a[2] = "third"
+	
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+	fmt.Println(a[2:3])
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+}
