11c434babe9f3e73ed7e491157445304d12a19e5
MikeH <matroxmike@icloud.com>
2017-01-05 14:38:23 +1100

    started work on display

---
+++ twoDgrid/twoDgrid.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+		var d [3]string  
+		d[1] = "1234567890"
+				d[0] = "00000000"
+	//[]rune( d[1:2]) = "m"
+		[]rune(d[0])[3] = []rune(d[1])[1]
+			fmt.Println(string([]rune(d[1])[1]))
+			fmt.Println(d, "c")
+	
+	names := [2]string{
+		"xxxxxxxxxx",
+		"yyyyyyyyyy",
+//		"George",
+//		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+//	b := names[1:3]
+	fmt.Println(a)
+
+//	b[0] = "XXX"
+	fmt.Println(a)
+	fmt.Println(" ")
+	fmt.Println(names)
+}
